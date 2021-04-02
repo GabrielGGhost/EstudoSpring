543aa39ad4e237ca67aab0526cda89f8badd7be0
Gabriel <gabriel.artioli@epiousion.com.br>
2021-04-02 20:02:09 -0300

    Método save estático adicionado

--- src/main/java/com/estudo/spring/SpringBoot2/service/AnimeService.java
+++ src/main/java/com/estudo/spring/SpringBoot2/service/AnimeService.java
@@ -7,16 +7,20 @@ import org.springframework.stereotype.Service;
 import org.springframework.web.server.ResponseStatusException;
 
 import java.util.Arrays;
+import java.util.LinkedList;
 import java.util.List;
+import java.util.concurrent.ThreadLocalRandom;
 
 @Service
 public class AnimeService {
 
-    List<Anime> animes = Arrays.asList(new Anime(1L,"One Piece"),
-            new Anime(2L,"Demon Slayer"),
-            new Anime(3L,"One Punch Man"),
-            new Anime(4L,"Pokemon"));
-
+    static List<Anime> animes;
+    static {
+        animes = new LinkedList<>(Arrays.asList(new Anime(1L, "One Piece"),
+                            new Anime(2L, "Demon Slayer"),
+                            new Anime(3L, "One Punch Man"),
+                            new Anime(4L, "Pokemon")));
+    }
     public List<Anime> listAll(){
         return animes;
     }
@@ -26,5 +30,11 @@ public class AnimeService {
                 .filter(anime-> anime.getId().equals(id))
                 .findFirst()
                 .orElseThrow(()-> new ResponseStatusException(HttpStatus.BAD_REQUEST, "Anime not found"));
+    }
+
+    public Anime save(Anime anime) {
+        anime.setId(ThreadLocalRandom.current().nextLong(3, 10000));
+        animes.add(anime);
+        return anime;
     }
 }
