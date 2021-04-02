2dcce613efa1846caee8b9039c4c1d13104f51d3
Gabriel <gabriel.artioli@epiousion.com.br>
2021-04-02 17:44:39 -0300

    Adicionado findbypk

--- src/main/java/com/estudo/spring/SpringBoot2/controller/AnimeController.java
+++ src/main/java/com/estudo/spring/SpringBoot2/controller/AnimeController.java
@@ -3,30 +3,30 @@ package com.estudo.spring.SpringBoot2.controller;
 import com.estudo.spring.SpringBoot2.service.AnimeService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.log4j.Log4j2;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import com.estudo.spring.SpringBoot2.domain.Anime;
-import com.estudo.spring.SpringBoot2.util.DateUtil;
 
-import java.time.LocalDateTime;
-import java.util.Arrays;
 import java.util.List;
+
 
 @RestController
 @RequestMapping("/animes")
 @Log4j2
 @RequiredArgsConstructor
 public class AnimeController {
-    private DateUtil dtUtil = new DateUtil();
+    final AnimeService animeService = new AnimeService();
 
     @GetMapping
-    public List<Anime> list(){
-        log.info(dtUtil.toDBDate(LocalDateTime.now()));
-        final AnimeService animeService = new AnimeService();
-        
-        return animeService.listAll();
+    public ResponseEntity<List<Anime>> list(){
+        return ResponseEntity.ok(animeService.listAll());
     }
 
-
+    @GetMapping("/{id}")
+    public ResponseEntity<Anime> findById(@PathVariable long id){
+        return ResponseEntity.ok(animeService.findById(id));
+    }
 }
--- src/main/java/com/estudo/spring/SpringBoot2/service/AnimeService.java
+++ src/main/java/com/estudo/spring/SpringBoot2/service/AnimeService.java
@@ -2,7 +2,9 @@ package com.estudo.spring.SpringBoot2.service;
 
 import com.estudo.spring.SpringBoot2.domain.Anime;
 import com.estudo.spring.SpringBoot2.repository.AnimeRepository;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
+import org.springframework.web.server.ResponseStatusException;
 
 import java.util.Arrays;
 import java.util.List;
@@ -10,10 +12,19 @@ import java.util.List;
 @Service
 public class AnimeService {
 
+    List<Anime> animes = Arrays.asList(new Anime(1L,"One Piece"),
+            new Anime(2L,"Demon Slayer"),
+            new Anime(3L,"One Punch Man"),
+            new Anime(4L,"Pokemon"));
+
     public List<Anime> listAll(){
-        return Arrays.asList(new Anime(1L,"One Piece"),
-                new Anime(2L,"Demon Slayer"),
-                new Anime(3L,"One Punch Man"),
-                new Anime(4L,"Pokemon"));
+        return animes;
+    }
+
+    public Anime findById(long id){
+        return animes.stream()
+                .filter(anime-> anime.getId().equals(id))
+                .findFirst()
+                .orElseThrow(()-> new ResponseStatusException(HttpStatus.BAD_REQUEST, "Anime not found"));
     }
 }
